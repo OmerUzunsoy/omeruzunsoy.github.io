8fcd1e08e206a0aaa8289f649669ce48da677125
Ömer Uzunsoy <uzunsoyomer@gmail.com>
2026-04-08 20:35:25 +0300

    Update heading texts in about-section.tsx

---
+++ about-section.tsx
@@ -0,0 +1,13 @@
+// About Section
+
+const AboutSection = () => {
+    return (
+        <div>
+            <h1>Fikirleri dijital</h1>
+            <h2>gerçekliğe dönüştüren kodcu</h2>
+            {/* Other content goes here */}
+        </div>
+    );
+};
+
+export default AboutSection;
